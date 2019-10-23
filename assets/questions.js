daaef7d6a45b61769546fc126d3e94d15160f821
Michael Trpak <mrtrpak3@gmail.com>
2019-10-22 22:53:16 -0400

    added questions for the quiz

---
+++ assets/questions.js
@@ -0,0 +1,30 @@
+var questions = [
+    {
+        title: "What does NOT go in the head of a html?",
+        choices: ["<title>", "style sheets", "<meta>", "<header>"],
+        answer: "header"
+    },
+    {
+        title: "What is content, padding, border and margin referring too?",
+        choices: ["HTML tags", "CSS Box Model", "CSS Selector", "Javascript Events"],
+        answer: "CSS Box Model"
+    },
+    {
+        title: "What tag is used to link to external style sheets?",
+        choices: ["<style>", "<head>", "<link>", "<div>"],
+        answer: "<link>"
+    },
+    {
+        title: "What will be the outcome of: '10' + 10 + 10 using Javascript",
+        choices: ["'101010'", "'20'10", "30", "'30'"],
+        answer: "'101010'"
+    },
+    {
+        title: "What is an array?",
+        choices: ["A variable that only holds one value at a time.",
+                "A function that that holds multiple values at one time.",
+                "One of the Javascript operators.",
+                "A special variable which holds more than one value at a time."                   
+    ]
+    }
+];
